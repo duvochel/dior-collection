289db3902c0294e0a3d6873c0ca1d8cd0c2e4d27
Francois <francois@devfullstack.com>
2023-09-12 15:46:50 +0200

    fix: icons & login css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import diorLogo from "../icons/diorLogo.svg";
+import cartIcon from "../icons/shopping-cart.svg";
+
 import "../styles/header.scss";
 
 const Header = () => {
@@ -8,12 +11,12 @@ const Header = () => {
     <div className="app-bar">
       <Link to="/">
         <div className="logo-header">
-          <img alt="logo-dior" src="icons/diorLogo.svg" />
+          <img alt="logo-dior" src={diorLogo} />
         </div>
       </Link>
       <Link to="/cart">
         <div className="cart-icon">
-          <img alt="shopping-cart" src="icons/shopping-cart.svg" />
+          <img alt="shopping-cart" src={cartIcon} />
         </div>
       </Link>
     </div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { useMutation, gql } from "@apollo/client";
 
 import { AUTH_TOKEN, IMG_EIFFEL } from "../constants";
+import diorLogo from "../icons/diorLogo.svg";
 
 import "../styles/login.scss";
 
@@ -36,7 +37,7 @@ const Login = () => {
   return (
     <div className="container-flex-v login-page">
       <div className="logo-center">
-        <img alt="logo-dior" src="icons/diorLogo.svg" />
+        <img alt="logo-dior" src={diorLogo} />
       </div>
       <div className="title-multi">
         <span>BIENVENUE</span>
@@ -75,8 +76,8 @@ const Login = () => {
         </div>
       </div>
 
-      <div>
-        <button onClick={login}>login</button>
+      <div >
+        <button onClick={login} className="btn-login">LOGIN</button>
       </div>
       <div className="bg-image">
         <img alt="eiffel" src={IMG_EIFFEL} />
